c35873240b134ecd855414f32ff4e7df740d2fec
Joern Turner <joern@existsolutions.com>
2023-03-08 16:15:24 +0100

    first shot

--- src/viewer.js
+++ src/viewer.js
@@ -1078,15 +1078,18 @@ $.extend( $.Viewer.prototype, $.EventSource.prototype, $.ControlDock.prototype, 
             //respond to touch events on the canvas, while the bottom half treated
             //them as touch events on the document body.  Thus we remove and store
             //the bodies elements and replace them when we leave full screen.
+/*
             this.previousBody = [];
             THIS[ this.hash ].prevElementParent = this.element.parentNode;
             THIS[ this.hash ].prevNextSibling = this.element.nextSibling;
             THIS[ this.hash ].prevElementWidth = this.element.style.width;
             THIS[ this.hash ].prevElementHeight = this.element.style.height;
+*/
             nodes = body.childNodes.length;
             for ( i = 0; i < nodes; i++ ) {
-                this.previousBody.push( body.childNodes[ 0 ] );
-                body.removeChild( body.childNodes[ 0 ] );
+                // this.previousBody.push( body.childNodes[ 0 ] );
+                // body.removeChild( body.childNodes[ 0 ] );
+                body.childNodes[ i ].classList.add('hidden');
             }
 
             //If we've got a toolbar, we need to enable the user to use css to
@@ -1140,6 +1143,12 @@ $.extend( $.Viewer.prototype, $.EventSource.prototype, $.ControlDock.prototype, 
             bodyStyle.display = this.bodyDisplay;
 
             body.removeChild( this.element );
+
+            body.querySelectorAll('.hidden').forEach(elem =>{
+                elem.classList.remove('hidden');
+            })
+
+/*
             nodes = this.previousBody.length;
             for ( i = 0; i < nodes; i++ ) {
                 body.appendChild( this.previousBody.shift() );
@@ -1150,6 +1159,7 @@ $.extend( $.Viewer.prototype, $.EventSource.prototype, $.ControlDock.prototype, 
                 this.element,
                 THIS[ this.hash ].prevNextSibling
             );
+*/
 
             //If we've got a toolbar, we need to enable the user to use css to
             //reset it to its original state
